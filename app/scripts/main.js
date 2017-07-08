2d5ded8e8a0c6893c89dd2c1076449f7980ff402
Zakeria Ali <zak.fined@gmail.com>
2017-07-08 03:52:13 +0100

    Add animation to key

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,19 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  const key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+
+  // Related to audio play
   if(!audio){ //if key clicked doesn't exist then stop function
     return; //stops function
   }
-  console.log(audio);
+  audio.currentTime = 0;
   audio.play(); //this plays the corresponding audio
+
+
+  // Related to key animation
+  key.classList.add('playing');
+
+  // Logging out info
+  console.log(audio);
+  console.log(key);
 });
