8db25c3e037785b145288d09fd6d124811447cdc
Zakeria Ali <zak.fined@gmail.com>
2017-07-09 03:58:54 +0100

    Reorgonise the code for the audio play and style

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
-window.addEventListener('keydown', function(e){
-  const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
-  const key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+function playSound(e){
+  var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  var key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
 
   // Related to audio play
   if(!audio){ //if key clicked doesn't exist then stop function
@@ -8,11 +8,23 @@ window.addEventListener('keydown', function(e){
   }
   audio.currentTime = 0;
   audio.play(); //this plays the corresponding audio
-
-
   // Related to key animation
   key.classList.add('playing');
+  
   // Logging out info
   console.log(audio);
   console.log(key);
+}
+
+
+var keys = document.querySelectorAll('.key');
+function removeTransition(){
+  this.classList.remove('playing');
+}
+
+keys.forEach(function (key) {
+  return key.addEventListener('transitionend', removeTransition);
 });
+
+
+window.addEventListener('keydown', playSound);
